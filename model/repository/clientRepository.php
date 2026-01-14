f72525b182971898e6e4d2e5ed8bd27fb3776d52
Achraf-podcast <achrafagourram6@gmail.com>
2026-01-14 21:52:52 +0100

    added userRepository

---
+++ model/repository/clientRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+class clientRepository
+{
+    public function insert (string $name, string $email, string $password): void
+    {
+        Database::request("INSERT INTO `users` (fullName, email, role, password) VALUES (?, ?, ?, ?)", [$name, $email, "client", $password]);
+    }
+}
+
+?>
