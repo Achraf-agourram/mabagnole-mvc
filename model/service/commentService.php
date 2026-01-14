405463e4ad2f856b6ee96e0094282c8ec636aa01
Achraf-podcast <achrafagourram6@gmail.com>
2026-01-15 00:46:04 +0100

    Added commentService

---
+++ model/service/commentService.php
@@ -0,0 +1,76 @@
+<?php
+
+class CommentService implements CommentServiceInterface
+{
+    private CommentRepository $commentRepository;
+
+    public function __construct(CommentRepository $commentRepository)
+    {
+        $this->commentRepository = $commentRepository;
+    }
+
+    public function addComment(string $commentText, int $idArticle, int $idClient): bool
+    {
+        try {
+            $this->commentRepository->insert($commentText, $idArticle, $idClient);
+            return true;
+
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+    public function editComment(Comment $comment, string $commentText, int $connectedUserId): bool
+    {
+        try {
+            if ($connectedUserId != $comment->idClient) {
+                return false;
+            }
+
+            $this->commentRepository->update($commentText, $comment->commentId);
+            return true;
+
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+    public function removeComment(Comment $comment, int $connectedUserId): bool
+    {
+        try {
+            if ($connectedUserId != $comment->idClient) {
+                return false;
+            }
+
+            $this->commentRepository->remove($comment->commentId);
+            return true;
+
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+    public function findComment($id): ?Comment
+    {
+        $result = $this->commentRepository->findById($id);
+
+        if (!$result) {
+            return null;
+        }
+
+        $comment = $result[0];
+
+        return new Comment($comment->commentId, $comment->commentText, $comment->idArticle, $comment->idClient);
+    }
+
+    public function getComments($idArticle): array
+    {
+        return $this->commentRepository->get($idArticle);
+    }
+
+    public function getCommentsTotalOfArticle($idArticle): int
+    {
+        $result = $this->commentRepository->getTotal($idArticle);
+        return $result[0]->total;
+    }
+}
