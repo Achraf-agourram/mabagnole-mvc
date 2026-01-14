5fe431dd8d912a6e9dad23783c1c506f451a0103
Achraf-podcast <achrafagourram6@gmail.com>
2026-01-14 21:31:47 +0100

    added commentServiceInterface

---
+++ model/service/commentServiceInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+interface commentServiceInterface
+{
+    public function addComment(string $commentText, int $idArticle, int $idClient): bool;
+
+    public function editComment(Comment $comment, string $commentText, int $connectedUserId): bool;
+
+    public function removeComment(Comment $comment, int $connectedUserId): bool;
+
+    public function findComment($id): ?Comment;
+
+    public function getComments($idArticle): array;
+
+    public function getCommentsTotalOfArticle($idArticle): int;
+}
+
+?>
