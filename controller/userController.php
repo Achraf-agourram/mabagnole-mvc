b4b2884a7a08503651b62566865d9514e58e6ae9
Achraf-podcast <achrafagourram6@gmail.com>
2026-01-15 12:26:31 +0100

    Completed userController

--- controller/userController.php
+++ controller/userController.php
@@ -8,6 +8,43 @@ class UserController
     {
         $this->userService = $userService;
     }
+
+    public function login (): void
+    {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $connectedUser = $this->userService->login($email, $password);
+        if($connectedUser)
+        {
+            $_SESSION['loggedAccount'] = $connectedUser->userId;
+            echo "logged successfully";
+            if ($connectedUser->role === 'admin') header('Location: admin/categories');
+            else header('Location: blog/explore');
+            exit;
+        }
+        else echo "please enter valid details";
+    }
+
+    public function checkAccess (?string $roleToCheck, string $redirectPage): void
+    {
+        if (!isset($_SESSION['loggedAccount']))
+        {
+            header("location: /login");
+            exit;
+        }
+
+        if (!$roleToCheck) header("location: /{$redirectPage}");
+        else
+        {
+            $connectedUser = $this->userService->findById($_SESSION['loggedAccount']);
+
+            if ($connectedUser->role === $roleToCheck) header("location: /{$redirectPage}");
+            else header("location: /login");
+        }
+
+        exit;
+    }
 }
 
 ?>
