e0e681c2f962f417c13a153bf0c990ee34e83bb4
Achraf-podcast <achrafagourram6@gmail.com>
2026-01-15 13:04:57 +0100

    added addArticle function to articleController

--- controller/articleContoller.php
+++ controller/articleContoller.php
@@ -2,7 +2,33 @@
 
 class ArticleController
 {
-    
+    private ArticleServiceInterface $articleService;
+    private TagServiceInterface $tagService;
+
+    public function __construct(ArticleServiceInterface $articleService, TagServiceInterface $tagService)
+    {
+        $this->articleService = $articleService;
+        $this->tagService = $tagService;
+    }
+
+    public function addArticle (): void
+    {
+        $title = $_POST['title'];
+        $image = $_FILES['image']['name'];
+        $tags = $_POST['tags'];
+        $content = $_POST['paragraph'];
+        $theme = $_POST['theme'];
+
+        if ($tags)
+        {
+            $tags = explode(" ", str_replace("#", "", $tags));
+            foreach($tags as $tag) if (!$this->tagService->existTag($tag)) $this->tagService->addTag($tag);
+
+        }else $tags = null;
+
+        if ($this->articleService->addArticle($title, $image, $tags, $content, $theme, $connectedUser->id)) echo "Article added successfully, wait until our support approuve it";
+        else echo "Adding article failed, please try again";
+    }
 }
 
 ?>
